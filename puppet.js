b4f726025cee3a2ad95b617c513cbae454d9be07
Jeff Sparrow <jeffsparrow.dev@gmail.com>
2022-08-31 22:49:32 -0500

    all orders displayed in table

--- puppet.js
+++ puppet.js
@@ -9,15 +9,27 @@ const scrapeData = async (username) => {
     await page.goto(process.env.URL);
 
 
-    //login form
-    await page.waitForTimeout(3000);
-    await page.screenshot({path: '1.png'});
+    //login
+    await page.waitForSelector('input');
     await page.type('input', process.env.USERNAME);
-
-
     await page.type('[type=password]', process.env.PASSWORD)
+    await page.click('[type=button]');
+    await page.waitForTimeout(5000);
 
-    await page.screenshot({path: '2.png'});
+    //navigate to orders page
+    await page.waitForSelector('.forceCommunityThemeNavTrigger')
+    await page.click('.forceCommunityThemeNavTrigger');
+    await page.waitForSelector('[id="1"]');
+    await page.click('[id="1"]');
+
+    //navigate to printable page
+    await page.goto(process.env.PRINT_URL);
+    await page.waitForTimeout(10000);
+    await page.screenshot({path: 'printable.png'});
+
+
+
+
 
     await browser.close();
   } catch (error) {
